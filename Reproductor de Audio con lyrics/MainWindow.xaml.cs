48c77e283ff0acaead5a8a68fc8fdc5829538a2b
alumno <alumno@E107>
2020-01-24 09:47:24 -0700

    Agregar archivos de proyecto.

---
+++ Reproductor de Audio con lyrics/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Reproductor_de_Audio_con_lyrics
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer media = new MediaPlayer();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            openmediafile();
+        }
+
+        private void openmediafile()
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            if (fdlg.ShowDialog() == true)
+                media.Open(new Uri(fdlg.FileName));
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+
+            
+       
+            timer.Start();
+            
+
+            
+        }
+
+
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+            Window1 win1 = new Window1();
+            win1.Show();
+            this.Close();
+
+            media.Play();
+
+        }
+    }
+}
